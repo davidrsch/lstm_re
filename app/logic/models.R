ad18c1baf581c9eacf6ee8abb53c9c42fb5ec944
davidrsch <daviddrsch@gmail.com>
2026-03-23 15:37:41 +0100

    refactor: convert LSTM model to Keras functional API (#4)
    Replace keras_model_sequential with keras_input + keras_model pattern.
    Simplify multi-layer logic by using return_sequences = layer < n_layers,
    eliminating nested if/else branches."

--- app/logic/models.R
+++ app/logic/models.R
@@ -1,42 +1,24 @@
 box::use(
-  keras3[keras_model_sequential, layer_dense, layer_lstm, layer_reshape],
+  keras3[keras_input, keras_model, layer_dense, layer_lstm, layer_reshape],
 )
 
 #' @export
 create_model <- function(structure, inputvec, outputvec) {
-  if (dim(structure)[2] == 1) {
-    model <- keras_model_sequential() |>
+  n_layers <- dim(structure)[2]
+  input <- keras_input(shape = c(dim(inputvec)[2], dim(inputvec)[3]))
+
+  x <- input
+  for (layer in seq_len(n_layers)) {
+    x <- x |>
       layer_lstm(
-        units = as.numeric(structure[[1]]),
-        input_shape = c(dim(inputvec)[2], dim(inputvec)[3])
+        units = as.numeric(structure[[layer]]),
+        return_sequences = layer < n_layers
       )
-  } else {
-    for (layer in seq_len(dim(structure)[2])) {
-      if (layer == 1) {
-        model <- keras_model_sequential() |>
-          layer_lstm(
-            units = as.numeric(structure[[1]]),
-            input_shape = c(dim(inputvec)[2], dim(inputvec)[3]),
-            return_sequences = TRUE
-          )
-      } else {
-        if (layer > 1 && layer < dim(structure)[2]) {
-          model <- model |>
-            layer_lstm(
-              units = as.numeric(structure[[layer]]),
-              return_sequences = TRUE
-            )
-        } else {
-          model <- model |>
-            layer_lstm(units = as.numeric(structure[[layer]]))
-        }
-      }
-    }
   }
 
-  model <- model |>
+  output <- x |>
     layer_dense(dim(outputvec)[2] * dim(outputvec)[3]) |>
     layer_reshape(c(dim(outputvec)[2], dim(outputvec)[3]))
 
-  model
+  keras_model(inputs = input, outputs = output)
 }
